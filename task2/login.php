d4888d971b4cf9c14e517ddb8421a836615b6686
LAI YEN PING <30991538+lyping123@users.noreply.github.com>
2026-07-31 10:40:17 +0800

    Disable password check and user registration
    Comment out password validation and user registration logic.

--- task2/login.php
+++ task2/login.php
@@ -23,17 +23,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit();
     }
 
-    if ($password === $confirm_password) {
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-        // $hashed_password=md5($password);
-        $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-        $stmt->bind_param("ss", $username, $hashed_password);
-        $stmt->execute();
-        $stmt->close();
-        echo "login successful!";
-    } else {
-        echo "Passwords do not match!";
-    }
+    // if ($password === $confirm_password) {
+    //     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    //     // $hashed_password=md5($password);
+    //     $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+    //     $stmt->bind_param("ss", $username, $hashed_password);
+    //     $stmt->execute();
+    //     $stmt->close();
+    //     echo "login successful!";
+    // } else {
+    //     echo "Passwords do not match!";
+    // }
 }
 ?>
 
